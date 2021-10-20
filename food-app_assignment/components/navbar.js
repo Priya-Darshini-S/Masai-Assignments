5864c0e06cee5a0b6b17aca98663f9540c153459
Priya_darshini_s <priya06200@gmail.com>
2021-10-20 12:58:57 +0530

    Create navbar.js

---
+++ food-app_assignment/components/navbar.js
@@ -0,0 +1,15 @@
+function navbar() {
+    return ` <div id="navbar">
+    <h3><a href="home.html">Home</a></h3>
+    
+    <div id="options">
+        <h3><a href="latest_receipe.html">Latest Receipe</a></h3>
+        <h3><a href="receipe_of_the_day.html">Receipe-of-the-day</a></h3>
+        <h3><a href="#">Sign In</a></h3>
+        <h3><a href="#">Sign up</a></h3>
+    </div>
+</div>`
+
+}
+export default navbar
+
